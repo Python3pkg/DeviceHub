510ea1add94b62b921ed171a67a7fb168b42f921
bustawin <xavier@bustawin.com>
2016-03-14 16:35:36 +0100

    Revert to standard method of getting the ip

--- app/event/prove_usage/hooks.py
+++ app/event/prove_usage/hooks.py
@@ -3,4 +3,4 @@ from flask import request
 
 def save_ip_of_request(prove_usages: list):
     for prove_usage in prove_usages:
-        prove_usage['ip'] = request.environ['REMOTE_ADDR']
+        prove_usage['ip'] = request.remote_addr
